c0e2d702570969ce864440efdb635ba93186f028
deneesh <catchdeneesh@gmail.com>
2020-10-22 12:36:56 -0500

    config

--- email/email.php
+++ email/email.php
@@ -8,40 +8,29 @@ use PHPMailer\PHPMailer\Exception;
 // Load Composer's autoloader
 require 'vendor/autoload.php';
 require 'secret.php';
-
 // Instantiation and passing `true` enables exceptions
 $mail = new PHPMailer(true);
 
 try {
     //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp.gmail.com'; //'smtp1.example.com';                    // Set the SMTP server to send through
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'catchdeneeshn@gmail.com'; //'user@example.com';                     // SMTP username
-    $mail->Password   = '$password'; //'secret';                               // SMTP password
+    $mail->Username   = 'catchdeneeshn@gmail.com';                     // SMTP username
+    $mail->Password   = '$password';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
     $mail->setFrom('catchdeneeshn@gmail.com', 'Mailer');
-    // $mail->setFrom('from@example.com', 'Mailer');
-    $mail->addAddress('catchdeneesh@live.com', 'Deneesh MS account');     // Add a recipient
-    // $mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
-    // $mail->addAddress('ellen@example.com');               // Name is optional
-    // $mail->addReplyTo('info@example.com', 'Information');
-    // $mail->addCC('cc@example.com');
-    // $mail->addBCC('bcc@example.com');
-
-    // Attachments
-    // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-
+    $mail->addAddress('catchdeneeshn@gmail.com', 'Deneesh');     // Add a recipient
+    
     // Content
     $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    $mail->Subject = 'This is subject';
+    $mail->Body    = 'This is message from Deneesh';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
     $mail->send();
     echo 'Message has been sent';
--- email/secret.php
+++ email/secret.php
@@ -1,1 +1,1 @@
-$password = 'deneesh'
+$password = ''
